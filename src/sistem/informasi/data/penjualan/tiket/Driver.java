1c2346394f4ba0f7747fb06812cf7b3d8eb2f87b
Herica Bunga M <hericabungamaharani@gmail.com>
2017-03-05 14:02:21 +0700

    Gerbong done

--- src/sistem/informasi/data/penjualan/tiket/Driver.java
+++ src/sistem/informasi/data/penjualan/tiket/Driver.java
@@ -15,7 +15,8 @@ public class Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Aplikasi a = new Aplikasi();
+        a.mainMenu();
     }
     
 }
--- src/sistem/informasi/data/penjualan/tiket/Gerbong.java
+++ src/sistem/informasi/data/penjualan/tiket/Gerbong.java
@@ -10,5 +10,47 @@ package sistem.informasi.data.penjualan.tiket;
  * @author USER
  */
 public class Gerbong {
+    private String kode;
+    private String jenis;
+
+    public Gerbong(String kode) {
+        this.kode = kode;
+    }
+
+    public Gerbong(String kode, String jenis) {
+        this.kode = kode;
+        this.jenis = jenis;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getJenis(String jenis) {
+        if (jenis.equals("Exc")) {
+            return ("Executive");
+        }
+        else if (jenis.equals("Bis")) {
+            return ("Business");
+        }
+        else if (jenis.equals("Eco")) {
+            return ("Economy");
+        } else {
+            return ("Jenis Gerbong Salah");
+        }
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    @Override
+    public String toString() {
+        return "Info Gerbong: " + "\n Kode Gerbong: " + kode + "\n Jenis Gerbong: " + getJenis(jenis);
+    }
     
 }
