a00273b8534d3106d596e6a5e6bfa53a1b41ca6b
kurniawisudaaji <kurniawisudaaji0@gmail.com>
2017-03-15 22:44:30 +0700

    Update Kereta dan Tiket
    Belum Selese dan belum fix

--- src/sistem/informasi/data/penjualan/tiket/Kereta.java
+++ src/sistem/informasi/data/penjualan/tiket/Kereta.java
@@ -7,8 +7,57 @@ package sistem.informasi.data.penjualan.tiket;
 
 /**
  *
- * @author USER
+ * @author KURNIA (1301150068)
  */
 public class Kereta {
+    private Gerbong[] daftarGerbong;
+    private String nama;
+    private String jenis;
+    private String jurusan;
+
+    public Kereta(Gerbong[] daftarGerbong, String nama, String jenis, String jurusan) {
+        this.daftarGerbong = daftarGerbong;
+        this.nama = nama;
+        this.jenis = jenis;
+        this.jurusan = jurusan;
+    }
+
+    public Gerbong[] getDaftarGerbong() {
+        return daftarGerbong;
+    }
+
+    public void setDaftarGerbong(Gerbong[] daftarGerbong) {
+        this.daftarGerbong = daftarGerbong;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    @Override
+    public String toString() {
+        return "Kereta{" + "daftarGerbong=" + daftarGerbong + ", nama=" + nama + ", jenis=" + jenis + ", jurusan=" + jurusan + '}';
+    }
+
     
 }
